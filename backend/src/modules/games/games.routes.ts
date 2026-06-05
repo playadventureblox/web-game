f45a168c4d4f0ee21741891e5801cd0a31634c51
NeuralicAI <oluwaseyidavid2005@gmail.com>
2026-06-05 15:49:00 +0100

    Update games.routes.ts

--- backend/src/modules/games/games.routes.ts
+++ backend/src/modules/games/games.routes.ts
@@ -3,6 +3,7 @@ import {
   getGames,
   getGameById,
   publishGame,
+  publishGameByPlaceId,
 } from "./games.controller.js";
 import { verifyToken } from "../../middleware/auth.middleware.js";
 
@@ -11,5 +12,6 @@ const router = Router();
 router.get("/", getGames);
 router.get("/:id", getGameById);
 router.post("/publish", verifyToken, publishGame);
+router.post("/publish-by-place-id", verifyToken, publishGameByPlaceId);
 
 export default router;
